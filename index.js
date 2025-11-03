18e649270708c95eb99591560277fe4b48c8bc8f
mdjunaidjewel <mdjunaidjewell@gmail.com>
2025-11-03 09:17:36 +0600

    Somethings

--- index.js
+++ index.js
@@ -9,8 +9,35 @@ app.use(cors());
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send("Hellow world!!");
+    res.send("The Server is Available");
 });
+
+const users = [{
+    name: "Sabana",
+    email: "sabana@gmail.com",
+    _id: 1,
+},
+{
+    _id: 2,
+    name: "Sabnoor",
+    email: "sabnoor@gmail.com"
+},
+{
+    _id: 3,
+    name: "Babita",
+    email: "babita@gmail.com"
+},
+{
+    _id: 4,
+    name: "Sakib Khan",
+    email: "sakibkhan@gmail.com"
+}
+];
+
+app.get('/users', (req, res)=> {
+    res.send(users)
+})
+
 
 app.listen(port, () => {
     console.log(`This is Port of ${port}`);
